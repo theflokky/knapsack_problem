3a87cf05eb51c6c443a4df146747cedb726140b6
PierreBonnefoy <pierreeta@hotmail.fr>
2022-11-08 17:49:04 +0100

    ant opti

--- ant_colony/ant_colony.py
+++ ant_colony/ant_colony.py
@@ -1,26 +1,25 @@
-import os.path
+import time
 import random
 import math
 
-# instances_01_KP/large_scale/knapPI_1_100_1000_1
+# test file = ../test_files/large_scale/knapPI_1_100_1000_1
+
+#/!\/!\ can be opti = you can try to modify it while you have the same structur
+#/!\/!\ do not change = destroy algo if you change it !
+#/!\/!\
 
 MINPHERO = 0.01
 MAXPHERO = 6
-RHO = 0.90
+RHO = 0.99
 ALPHA = 1
 BETA = 5
 
 
-def printObejects(objects):
-    for i in objects.items() :
-        print(i)
-    print("\n")
-
-def defCandi(candidates) :
+def defCandi(candidates) : # can be opti ?
     for i in objects.items():
         candidates.update({i[0] : 1})
 
-def defPhero(phero) : 
+def defPhero(phero) : # can be opti ?
     for j in objects.items():
         tmp = []
         tmp.append(0)
@@ -34,35 +33,37 @@ def defPhero(phero) :
     phero.update({0 : tmp})
     
 
-def sumPheroValue(phero,objects,candidates,actu) :
+def sumPheroValue(phero,objects,candidates,current) : # do not change !!!
+    # Sum of pheromones depending of the curent item
     sum=0
     for i in objects.items() :
         if candidates[i[0]] == 1 :
-            sum += math.pow(phero[actu][i[0]],ALPHA)*math.pow((objects[i[0]][0]/objects[i[0]][1]),BETA)
+            sum += math.pow(phero[current][i[0]],ALPHA)*math.pow((objects[i[0]][0]/objects[i[0]][1]),BETA) 
     return sum
 
-def updatePhero(phero): 
+def updatePhero(phero): # do not change !!!
+    # Evaporation of pheromones 
+    # The min quantity of pheromones is MINPHERO
     for i in phero.items() :
-        tmp = []
         
         if phero[i[0]][0]*RHO < MINPHERO :
-            tmp.append(MINPHERO)
+            phero[i[0]][0] = MINPHERO
         else :
-            tmp.append(phero[i[0]][0]*RHO)
+            phero[i[0]][0] = phero[i[0]][0]*RHO
+            
         for j in phero.items():
             if i[0] == j[0] :
-                tmp.append(0)
+                phero[i[0]][j[0]] = 0
             elif phero[i[0]][j[0]]*RHO < MINPHERO :
-                tmp.append(MINPHERO)
+                phero[i[0]][j[0]] = MINPHERO
             else :
-                tmp.append(phero[i[0]][j[0]]*RHO)
-        phero.update({i[0] : tmp})
-
-
-def setPhero(phero,bestSolution,result):
-    p = 1/(1+bestSolution['value']-result['value'])
+                phero[i[0]][j[0]] = phero[i[0]][j[0]]*RHO
+                
+def setPhero(phero,bestSolution,currentSolution): # do not change !!!
+    # The quantity of pheromones set on the solution by ants depending on value and weigth of the solution and the best solution
+    p = 1/(1+bestSolution['value']-currentSolution['value'])
     prec = 0
-    for i in result["number"] :
+    for i in currentSolution["number"] :
         if phero[i][prec]+p > MAXPHERO :
             phero[i][prec] = MAXPHERO
             phero[prec][i] = MAXPHERO
@@ -71,16 +72,16 @@ def setPhero(phero,bestSolution,result):
             phero[prec][i] = phero[prec][i]+p
         prec = i
 
-def setProb(probabilites, phero ,objects,candidates,actu):
-    p = sumPheroValue(phero,objects,candidates,actu)
+def setProb(probabilities, phero ,objects,candidates,current): # do not change !!!
+    # Calculate the probabilities for each items depending of the current item
+    p = sumPheroValue(phero,objects,candidates,current)
     maxProb = 0
     for i in candidates.items():
+        # Calculate only for items candidates
         if i[1] == 1 :
             minProb = maxProb
-            maxProb += (math.pow(phero[actu][i[0]],ALPHA)*math.pow((objects[i[0]][0]/objects[i[0]][1]),BETA))/p
-            probabilites.update({i[0] : [minProb, maxProb]})
-        else :
-            probabilites.update({i[0] : [-1, -1]}) # modif
+            maxProb += (math.pow(phero[current][i[0]],ALPHA)*math.pow((objects[i[0]][0]/objects[i[0]][1]),BETA))/p
+            probabilities.update({i[0] : [minProb, maxProb]})
 
 while True:
     try :
@@ -88,12 +89,12 @@ while True:
         print("\nTo quit : CTRL-C")
         name = input("File name : ")
         
-        # Load file data ----------------------------------
+        # Load file data --------------------------------------
         with open(name, "r") as f :
             allFile = f.read()
             line = allFile.split("\n")
             
-        # Split file data ---------------------------------
+        # Split file data -------------------------------------
         line = line[:-2] # Don't take last line (the solution)
         nb, w = line[0].split()
         n = int(nb)   # Number of items
@@ -101,85 +102,117 @@ while True:
         line = line[1:] # Don't take first line (n, wmax)
 
         objects = {}
-        allValue = 0
-        allW = 0
         i = 0
         for l in line :
             # Put all objects in 'objects'
             value, w = l.split()
             i+=1
             objects.update({i : [int(value),int(w)]})
-            allValue = allValue + int(value)
-            allW = allW + int(w)
-          
-
-        phero = {}
-        defPhero(phero)
-
-        # Create the probabilities table 
-        probabilities = {}
-
-        bestSolution ={'number' :[], 'value' : -1 , 'weight': -1}
 
         nbAnts = input("How many ants : ")
 
-        
-        for ant in range(int(nbAnts)) :
+        # For the time of execution
+        start_process = time.time()
 
-            result = {'number' :[], 'value' : 0 , 'weight': 0}
-            allValueTmp = allValue
+        # Initialize the pheromones table ---------------------
+        phero = {}
+        defPhero(phero)
+
+        # Create the probabilities table ----------------------
+        probabilities = {}
+
+        # Create the save of the best solution ----------------
+        bestSolution ={'number' :[], 'value' : -1 , 'weight': -1}
+        
+        # Ant loop --------------------------------------------
+        for ant in range(int(nbAnts)) :
+            # Create the current solution
+            currentSolution = {'number' :[], 'value' : 0 , 'weight': 0}
             
             # Create candidates list
             candidates = {}
             defCandi(candidates)
 
+            # Number of items check
             cantGo = 0
-            actu = 0
+
+            # Set the current item to 0 (empty bag)
+            current = 0
+
             #First item
             x = random.random()
-            setProb(probabilities,phero,objects,candidates,actu)
+
+            # Set probabilities for 0 (empty bag)
+            setProb(probabilities,phero,objects,candidates,current)
+            
+            # Get the first item 
             for j in probabilities.items():
-                if x >= j[1][0] and x < j[1][1] and (result["weight"]+objects[j[0]][1]) <= wmax:
+                if x >= j[1][0] and x < j[1][1] and (currentSolution["weight"]+objects[j[0]][1]) <= wmax:
+                    # Remove j of cantidates
                     candidates.update({j[0] : 0})
+                    # Remove the pobability of j
+                    probabilities.update({j[0] : [-1, -1]}) # do not change !!!
+                    # Save j in the current solution
+                    currentSolution["number"].append(j[0])
+                    currentSolution["value"] += objects[j[0]][0]
+                    currentSolution["weight"] += objects[j[0]][1]
+                    # Put j in the current item
+                    current = j[0]
+                    # Incrase item check
                     cantGo += 1
-                    actu = j[0]
-                    result["number"].append(j[0])
-                    result["value"] += objects[j[0]][0]
-                    result["weight"] += objects[j[0]][1]
+                    # If we found the item we don't need to check the others
                     break
-                elif (result["weight"]+objects[j[0]][1]) > wmax :
-                    candidates.update({j[0] : 0})
-                    cantGo += 1
-
+            
+            # While we didin't check all items
             while cantGo<=n:
+                # Next item
                 x = random.random()
-                setProb(probabilities,phero,objects,candidates,actu)
+                # Update probabilities for the current item
+                setProb(probabilities,phero,objects,candidates,current)
                 for j in probabilities.items():
-                    if x >= j[1][0] and x < j[1][1] and (result["weight"]+objects[j[0]][1]) <= wmax:
+                    # Get the next item
+                    if x >= j[1][0] and x < j[1][1] and (currentSolution["weight"]+objects[j[0]][1]) <= wmax:
+                        # Remove j of candidates
                         candidates.update({j[0] : 0})
+                        # Remove the probability of j
+                        probabilities.update({j[0] : [-1, -1]}) # do not change !!!
+                        # Save j in the current item
+                        currentSolution["number"].append(j[0])
+                        currentSolution["value"] += objects[j[0]][0]
+                        currentSolution["weight"] += objects[j[0]][1]
+                        # Put j in the current item
+                        current = j[0]
+                        # Incrase item check
                         cantGo += 1
-                        actu = j[0]
-                        result["number"].append(j[0])
-                        result["value"] += objects[j[0]][0]
-                        result["weight"] += objects[j[0]][1]
+                        # If xe found the item we don't need to check the others
                         break
-                    elif (result["weight"]+objects[j[0]][1]) > wmax :
+                    # If the items is too heavy
+                    elif (currentSolution["weight"]+objects[j[0]][1]) > wmax :
+                        # Remove j of candidates
                         candidates.update({j[0] : 0})
+                        # Remove the probability of j
+                        probabilities.update({j[0] : [-1, -1]}) # do not change !!!
+                        # Incrase item check
                         cantGo += 1
-                    
-            if result["value"] > bestSolution["value"] :
-                bestSolution = result
             
-            #printObejects(phero)
+            # Check if the current solution is better than the best solution
+            if currentSolution["value"] > bestSolution["value"] :
+                # Save the current solution in best solution
+                bestSolution = currentSolution
+            
+            # Evaporation of pheromones
             updatePhero(phero)
-            #printObejects(phero)
-            setPhero(phero,bestSolution,result)
-            
+            # Set pheromones on the current solution
+            setPhero(phero,bestSolution,currentSolution)
+        
+        # For time of execution
+        end_process = time.time()
 
+        # Just to show the best solution 
         sorted = bestSolution["number"]
         sorted.sort()
         print(sorted)
-        print(f"{bestSolution['value']}\nweith : {bestSolution['weight']}/{wmax}\n")
+        print(f"{bestSolution['value']}\nweith : {bestSolution['weight']}/{wmax}\ntime : {end_process-start_process}\n")
                 
 
     except KeyboardInterrupt:
