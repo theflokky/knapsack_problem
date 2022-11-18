61c47c12137bb9337840d75e38c0471220841d88
theflokky <aloyslanapro@gmail.com>
2022-11-18 11:46:21 +0100

    [DOC] Rajout de Commentaires au code

--- brute_force/brute_force.py
+++ brute_force/brute_force.py
@@ -8,6 +8,8 @@ solution = [0, 0, []]
 
 # Functions
 
+# Function wich is calculating all the possibilities of combination of element and returned a list of 0 and 1
+
 
 def generateAllPossibilities(n):
     l = [[0], [1]]
@@ -20,27 +22,48 @@ def generateAllPossibilities(n):
         start = tmp
     return l[start:]
 
+# Function principal of the bruteForve
+
 
 def bruteForce(maxWeigth, objectList):
+    # We launch the timer
     startTime = time.time()
-    l = generateAllPossibilities(len(objectList))
-    current_best = (0, 0)
-    for current_possibility, element in enumerate(l):
-        current_weight = 0
-        current_value = 0
+
+    # We generate all the possibilities
+    allPossibilities = generateAllPossibilities(len(objectList))
+
+    # Initialization of the current best solution
+    currentBest = (0, 0)
+
+    # We parcour all the possibilities
+    for currentPossibility, element in enumerate(allPossibilities):
+        # Initialization of the current Weigth and Value
+        currentWeigth = 0
+        currentValue = 0
+
+        # We parcours the possibility list
         for index, bit in enumerate(element):
+            # If equals to 1
             if bit:
-                current_weight += objectList[index][1]
-                if current_weight <= maxWeigth:
-                    current_value += objectList[index][0]
+                # We update the currentWeigth
+                currentWeigth += objectList[index][1]
+                # We verify that the current Solution can enter into the knapsack
+                if currentWeigth <= maxWeigth:
+                    currentValue += objectList[index][0]
+                # If not we pass to next one
                 else:
                     break
-        if current_value > current_best[1]:
-            current_best = (current_possibility, current_value)
+        # We verify if the current solution is better than the actual better one
+        if currentValue > currentBest[1]:
+            currentBest = (currentPossibility, currentValue)
+
+    # Ending the timer
     endTime = time.time()
+
+    # Reconstitution of the knapsack in order to facilitate the displaying
     finalKnapsack = []
-    for i, obj in enumerate(l[current_best[0]]):
+    for i, obj in enumerate(allPossibilities[currentBest[0]]):
         if (obj == 1):
             finalKnapsack.append(objectList[i])
 
-    return (endTime - startTime), finalKnapsack, current_best[1]
+    return (endTime - startTime), finalKnapsack, currentBest[1]
