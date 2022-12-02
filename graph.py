f3e6f51bc0ce4959a183a20bbb6ab813044a12bc
Ophel <ophelie.marechal2.@etu.univ-st-etienne.fr>
2022-12-02 11:59:57 +0100

    graph correction

--- graph.py
+++ graph.py
@@ -13,7 +13,7 @@ personalAlgo = "'Personal Algorithm'"
 
 def graph(time, tests):
 
-    print("\n brute_force :     ", time["brute_force"], "\n branch_and_bound :", time["branch_and_bound"], "\n greedy :          ", time["greedy"], "\n greedy_value :      ", time["greedy_value"], "\n greedy_weight :      ", time["greedy_weight"], "\n dynamic :         ", time["dynamic_programming"], "\n polynomial :      ", time["polynomial"], "\n grasp :            ", time["grasp"], "\n ant_colony :      ", time["ant"], "\n personal :        ", time["personal"], "\n\n test files :      ", tests, flush=True)
+    print("\n brute_force :     ", time["brute_force"], "\n branch_and_bound :", time["branch_and_bound"], "\n greedy :          ", time["greedy"], "\n greedy_value :      ", time["greedy_value"], "\n greedy_weight :      ", time["greedy_weight"], "\n dynamic :         ", time["dynamic_programming"], "\n polynomial :      ", time["polynomial"], "\n grasp :            ", time["grasp"], "\n ant_colony :      ", time["ant_colony"], "\n personal :        ", time["personal"], "\n\n test files :      ", tests, flush=True)
     print()
 
 
@@ -42,8 +42,8 @@ def graph(time, tests):
     if len(time["grasp"]) != 0 :
         plt.plot(tests, time["grasp"], label = 'Grasp', marker = '.', color = 'c')
     
-    if len(time["ant"]) != 0 :
-        plt.plot(tests, time["ant"], label = 'Ant colony', marker = '.', color = 'hotpink')
+    if len(time["ant_colony"]) != 0 :
+        plt.plot(tests, time["ant_colony"], label = 'Ant colony', marker = '.', color = 'hotpink')
 
     if len(time["personal"]) != 0 :
         plt.plot(tests, time["personal"], label = personalAlgo, marker = '.', color = 'y')
