dd466c018ae494527ac205194d4906595ce5dad5
Ophel <ophelie.marechal2.@etu.univ-st-etienne.fr>
2022-12-02 12:18:20 +0100

    graph correction

--- graph.py
+++ graph.py
@@ -13,7 +13,7 @@ personalAlgo = "'Personal Algorithm'"
 
 def graph(time, tests):
 
-    print("\n brute_force :     ", time["brute_force"], "\n branch_and_bound :", time["branch_and_bound"], "\n greedy :          ", time["greedy"], "\n greedy_value :      ", time["greedy_value"], "\n greedy_weight :      ", time["greedy_weight"], "\n dynamic :         ", time["dynamic_programming"], "\n polynomial :      ", time["polynomial"], "\n grasp :            ", time["grasp"], "\n ant_colony :      ", time["ant_colony"], "\n personal :        ", time["personal"], "\n\n test files :      ", tests, flush=True)
+    print("\n brute_force :     ", time["brute_force"], "\n branch_and_bound :", time["branch_and_bound"], "\n greedy :          ", time["greedy"], "\n greedy_value :    ", time["greedy_value"], "\n greedy_weight :   ", time["greedy_weight"], "\n dynamic :         ", time["dynamic_programming"], "\n polynomial :      ", time["polynomial"], "\n grasp :           ", time["grasp"], "\n ant_colony :      ", time["ant_colony"], "\n personal :        ", time["personal"], "\n\n test files :      ", tests, flush=True)
     print()
 
 
@@ -28,10 +28,10 @@ def graph(time, tests):
         plt.plot(tests, time["greedy"], label = 'Greedy', marker = '.', color = 'limegreen')
 
     if len(time["greedy_value"]) != 0 :
-        plt.plot(tests, time["greedy_value"], label = 'Greedy Value', marker = '.', color = 'c')
+        plt.plot(tests, time["greedy_value"], label = 'Greedy Value', marker = '.', color = 'tan')
 
     if len(time["greedy_weight"]) != 0 :
-        plt.plot(tests, time["greedy_weight"], label = 'Greedy Weight', marker = '.', color = 'c')
+        plt.plot(tests, time["greedy_weight"], label = 'Greedy Weight', marker = '.', color = 'peachpuff')
 
     if len(time["dynamic_programming"]) != 0 :
         plt.plot(tests, time["dynamic_programming"], label = 'Dynamic', marker = '.', color = 'r')
@@ -52,7 +52,7 @@ def graph(time, tests):
     plt.ylabel(yLabel)
     plt.xlabel(xLabel)
 
-    plt.title(title, fontname = "Times New Roman", fontsize = 15, fontweight = "bold", y = 1.02)
+    plt.title(title, fontname = "Times New Roman", fontsize = 15, fontweight = "bold", y = 1.01)
 
     plt.legend()
     plt.show()
